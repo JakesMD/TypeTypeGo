28f81a09f32e5d2261c71292762348f39fb502de
JakesMD <jakesmd5@gmail.com>
2021-03-12 15:19:41 +0100

    Add difficulty indicator

--- lib/screens/main/textInputBox.dart
+++ lib/screens/main/textInputBox.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:typetypego/config/config.dart';
@@ -69,7 +71,7 @@ class TextInputBox extends StatelessWidget {
 }
 
 /// The text field displayed in the [TextInputBox].
-class TextInputBoxTextField extends StatelessWidget {
+class TextInputBoxTextField extends StatefulWidget {
   final TextEditingController? controller;
   final FocusNode focusNode;
   final GlobalKey formKey;
@@ -82,14 +84,74 @@ class TextInputBoxTextField extends StatelessWidget {
       required this.maxWordLength});
 
   @override
+  _TextInputBoxTextFieldState createState() => _TextInputBoxTextFieldState();
+}
+
+class _TextInputBoxTextFieldState extends State<TextInputBoxTextField> {
+  String _value = "";
+  String _difficulty = "VERY EASY";
+
+  /// Calculates the difficulty of the [value] and updates [_value] and [_difficulty] accordingly.
+  void _updateDifficulty(String value) {
+    setState(() {
+      final String trimmedValue = value.trim();
+      _value = trimmedValue;
+
+      // Set the difficuly text to "VERY EASY" is the textbox is empty.
+      if (trimmedValue.isEmpty)
+        _difficulty = "VERY EASY";
+
+      // True if the textbox is not empty.
+      else {
+        // Create a list of all the easy characters.
+        final List<String> easyCharacters =
+            "abcdefghijklmnopqrstuvwxyz".split("")..addAll([" ", "\n"]);
+
+        double difficultyPoints = 0;
+
+        // Loop through the text in the textbox.
+        trimmedValue.split("").forEach((String element) {
+          // True if the character is not easy.
+          if (!easyCharacters.contains(element)) {
+            // Add 0.25 to difficultPoints if the character is a capital letter.
+            if (easyCharacters.contains(element.toLowerCase()))
+              difficultyPoints += 0.25;
+            // Add 0.5 to difficultPoints if the character is a number.
+            else if (double.tryParse(element) != null)
+              difficultyPoints += 0.5;
+            // Add 1 to difficultPoints if the character is a anything else, e.g. !, :, #,...
+            else
+              difficultyPoints++;
+          }
+        });
+
+        // Calculate the difficulty by dividing the points by the square root of the length of text.
+        final double difficulty = difficultyPoints / sqrt(trimmedValue.length);
+
+        // Update the difficuly text according to the calculated difficulty.
+        if (difficultyPoints < 0.75)
+          _difficulty = "VERY EASY";
+        else if (difficulty < 1)
+          _difficulty = "EASY";
+        else if (difficulty < 1.25)
+          _difficulty = "MEDIUM";
+        else if (difficulty < 1.5)
+          _difficulty = "HARD";
+        else
+          _difficulty = "VERY HARD";
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Form(
-      key: formKey,
+      key: widget.formKey,
       child: SizedBox(
         height: 200,
         child: TextFormField(
-          controller: controller,
-          focusNode: focusNode,
+          controller: widget.controller,
+          focusNode: widget.focusNode,
           validator: (value) {
             final String trimmedValue = value!.trim();
             // True if the text field is empty.
@@ -104,11 +166,12 @@ class TextInputBoxTextField extends StatelessWidget {
                     .reduce((value, element) =>
                         value.length > element.length ? value : element)
                     .length >
-                maxWordLength) {
+                widget.maxWordLength) {
               return 'A word is too long (try making your window bigger)';
             }
             return null;
           },
+          onChanged: _updateDifficulty,
           maxLines: null,
           minLines: null,
           expands: true,
@@ -123,6 +186,7 @@ class TextInputBoxTextField extends StatelessWidget {
                 color: Palette.blueGrey.withOpacity(0.5), fontSize: 16),
             errorStyle: TextStyle(color: Palette.red, fontSize: 16),
             counterStyle: TextStyle(color: Palette.blueGrey, fontSize: 16),
+            counterText: "Difficulty: $_difficulty  -  ${_value.length}/1000",
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(Config.borderRadius)),
             errorBorder: OutlineInputBorder(
