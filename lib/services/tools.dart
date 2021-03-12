f87728bdd793fb21368de884f15eeffa8d007157
JakesMD <jakesmd5@gmail.com>
2021-03-12 15:36:59 +0100

    Fix stopwatch millis bug

--- lib/services/tools.dart
+++ lib/services/tools.dart
@@ -15,7 +15,7 @@ class Tools {
     var secs = milliseconds ~/ 1000;
     var minutes = ((secs % 3600) ~/ 60).toString().padLeft(2, '0');
     var seconds = (secs % 60).toString().padLeft(2, '0');
-    var millis = (milliseconds % 10).toString().padLeft(2, '0');
+    var millis = (milliseconds % 100).toString().padLeft(2, '0');
     return "$minutes:$seconds:$millis";
   }
 }
